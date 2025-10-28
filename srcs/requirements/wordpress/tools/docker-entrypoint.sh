6f775ee64ba629401637a2a76b7f82d97e7637fa
yeonjunky <yeonjunky@gmail.com>
2025-10-28 11:30:37 +0900

    create author user after initialize wordpress

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -6,13 +6,10 @@ echo '[Entrypoint] checking /var/www/html'
 if ! wp core is-installed 2>/dev/null; then
   wp core install --url=yeonjuki.42.fr --title=yeonjuki-inception --admin_user=yeonjunky --admin_password=password \
     --admin_email=yeonjunky@42.fr --skip-email --allow-root
-  # cp ./wp-config.php /var/www/html/
+  wp user create yeonjuki yeonjuki@42.fr --role=author --user_pass=password --allow-root
   chown -R www-data:www-data /var/www/html
 fi
 
-wp user create yeonjuki yeonjuki@42.fr --role=administrator --user_pass=password --allow-root
-
 echo '[Entrypoint] ended'
 
-# 반드시 exec로 CMD 실행
 exec "$@"
