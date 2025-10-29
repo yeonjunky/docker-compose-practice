6d72ee1dca3d91edaba972727bb99a4aa588afd6
yeonjunky <yeonjunky@gmail.com>
2025-10-29 08:01:39 -0400

    local_root=/var/www/html does not work

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -1,4 +1,16 @@
-if ! id "ftpuser" >/dev/null &2>1; then
-    adduser ftpuser
-    chown -R ftpuser:ftpuser /var/www/html
+#!/bin/bash
+
+set -e
+
+if ! id "${FTP_USER}" >/dev/null 2>&1; then
+    useradd -m -d /var/www/html -s /usr/sbin/nologin "${FTP_USER}"
+    chown -R "${FTP_USER}":"${FTP_USER}" /var/www/html
+    echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 fi
+
+if ! ls /etc/vsftpd.userlist >/dev/null 2>&1; then
+    touch /etc/vsftpd.userlist
+    echo "${FTP_USER}" > /etc/vsftpd.userlist
+fi
+
+exec "$@"
