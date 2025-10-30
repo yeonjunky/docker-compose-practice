75e177a618909a4f14ad072bc615f82d20a65056
yeonjunky <yeonjunky@gmail.com>
2025-10-30 01:23:40 -0400

    use environment variable to specify wordpress version

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 if ! wp core is-installed --allow-root 2>/dev/null; then
   echo '[Entrypoint] Wordpress files are missing. installing...'
-  wp core download --force --version=6.8 --allow-root
+  wp core download --force --version=${WORDPRESS_VERSION} --allow-root
   wp core install --url='http://yeonjuki.42.fr' --title=yeonjuki-inception --admin_user=yeonjunky --admin_password=password \
     --admin_email=yeonjunky@42.fr --skip-email --allow-root
   wp user create yeonjuki yeonjuki@42.fr --role=author --user_pass=password --allow-root
